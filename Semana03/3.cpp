2956150839b789d9641bad7673a8a4319c8c86a9
Ian <ian.cardoso@iupay.com.br>
2021-03-24 17:01:29 -0300

    Add problem 3

---
+++ Semana03/3.cpp
@@ -0,0 +1,28 @@
+// C++ recursive function to
+// solve tower of hanoi puzzle
+#include <bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int n, char from_rod,
+                    char to_rod, char aux_rod)
+{
+    if (n == 1)
+    {
+        cout << "Move disk 1 from rod " << from_rod <<
+                            " to rod " << to_rod<<endl;
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod <<
+                                " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+// Driver code
+int main(int argc, char* argv[])
+{
+
+    int n= atoi(argv[1]);
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+    return 0;
+}
